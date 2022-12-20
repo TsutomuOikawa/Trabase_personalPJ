f185247f2c14878115a478bad41d3a1f1c93923d
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-12-20 10:25:11 +0900

    add: notesテーブル作成とダミーデータ流し込み

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->numberBetween($min = 1, $max = 5),
+            'pref_id' => 4,
+            'title' => $this->faker->realText(15),
+            'created_at' => date('Y-m-d H:i:s')
+        ];
+    }
+}
---
+++ database/migrations/2022_12_19_074838_create_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id('note_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('pref_id');
+            $table->string('title');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
