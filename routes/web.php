28722c38e45ec2c21f6ce4c0c700c0fff145be2f
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-12-22 23:59:38 +0900

    Create: index.blade.php
    index.bladeを追加し、ホーム画面に設定

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // ノート作成
+    Route::get('/notes/create', [\App\Http\Controllers\NotesController::class, 'showForm']) -> name('notes.showForm');
+    Route::post('/notes/create', [\App\Http\Controllers\NotesController::class, 'create']) -> name('notes.create');
+});
+
+require __DIR__.'/auth.php';
 
 // ノート閲覧
 Route::get('/notes', [\App\Http\Controllers\NotesController::class, 'showList']) -> name('notes.showList');
-// ノート作成
-Route::get('/notes/create', [\App\Http\Controllers\NotesController::class, 'showForm']) -> name('notes.showForm');
-Route::post('/notes/create', [\App\Http\Controllers\NotesController::class, 'create']) -> name('notes.create');
