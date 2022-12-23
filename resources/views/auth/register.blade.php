a5d9badaff97e9bfdee8f2158ed5b250dc328791
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-12-23 14:57:30 +0900

    update: auth/register.blade.php
    各inputフォームの名前をLaravel Breeze標準仕様に合わせた。
    エラーメッセージのbladeテンプレート化
    不要になったデフォルトのフォームの削除

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,82 +8,36 @@
     <div class="container_body--xs">
       <form method="POST" action="{{ route('register') }}" class="form">
       @csrf
-
-      <h2 class="form_title">ご登録フォーム</h2>
-
         <label>
           <div class="form_name">
             <span class="form_label form_label--required">必須</span>
             メールアドレス
           </div>
-          <input type="text" name="email" class="form_input" value="" placeholder="example@test.com">
+          <input type="text" name="email" class="form_input @error('email') form_input--err @enderror" value="" placeholder="example@test.com">
         </label>
-        <p class="form_errMsg"></p>
-
+        <p class="form_errMsg">@error('email') {{ $message }} @enderror</p>
         <label>
           <div class="form_name">
             <span class="form_label form_label--required">必須</span>
             パスワード
             <span class="smallfont">（半角英数字6文字以上）</span>
           </div>
-          <input type="password" name="pass" class="form_input" value="">
+          <input type="password" name="password" class="form_input @error('password') form_input--err @enderror" value="">
         </label>
-        <p class="form_errMsg"></p>
-
+        <p class="form_errMsg">@error('password') {{ $message }} @enderror</p>
         <label>
           <div class="form_name">
             <span class="form_label form_label--required">必須</span>
             パスワード再入力
           </div>
-          <input type="password" name="pass_re" class="form_input" value="">
+          <input type="password" name="password_confirmation" class="form_input @error('password_confirmation') form_input--err @enderror" value="">
         </label>
-        <p class="form_errMsg"></p>
-
+        <p class="form_errMsg">@error('password_confirmation') {{ $message }} @enderror</p>
         <button type="submit" class="form_button" name="">登録する</button>
-
-        <p class="form_notion"><a href="login.php" class="link"><span style="font-size:18px;">&gt</span> 登録済みの方はこちら</a></p>
-
-
-  
-
-          <!-- Email Address -->
-          <div class="mt-4">
-              <x-input-label for="email" :value="__('Email')" />
-              <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-              <x-input-error :messages="$errors->get('email')" class="mt-2" />
-          </div>
-
-          <!-- Password -->
-          <div class="mt-4">
-              <x-input-label for="password" :value="__('Password')" />
-
-              <x-text-input id="password" class="block mt-1 w-full"
-                              type="password"
-                              name="password"
-                              required autocomplete="new-password" />
-
-              <x-input-error :messages="$errors->get('password')" class="mt-2" />
-          </div>
-
-          <!-- Confirm Password -->
-          <div class="mt-4">
-              <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-              <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                              type="password"
-                              name="password_confirmation" required />
-
-              <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-          </div>
-
-          <div class="flex items-center justify-end mt-4">
-              <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                  {{ __('Already registered?') }}
-              </a>
-
-              <x-primary-button class="ml-4">
-                  {{ __('Register') }}
-              </x-primary-button>
-          </div>
+        <p class="form_notion"><a href="{{ route('login') }}" class="link">&gt 登録済みの方はこちら</a></p>
       </form>
+    </div>
+  </div>
+</main>
+
 @endsection
