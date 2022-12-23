0fdced93c15974714f82d922608abedc25ccb7f9
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-12-23 14:53:08 +0900

    update: profile/edit.blade.php
    プロフィール編集画面を修正
    通信方法等はLaravel Breezeの標準仕様を継承
    
    todo: カルーセル化、一次退会申請後のモーダル表示

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,101 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
+@section('title', 'プロフィール編集｜Trabase（トラベス）')
+@section('content')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+<main class="page-wrapper">
+  <div class="container--baseColor">
+    <h1 class="container_title">プロフィール編集</h1>
+    <div class="container_body--xs container_body--carousel">
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+      <form method="post" action="{{ route('profile.update') }}" class="form form--carousel">
+        @csrf
+        @method('patch')
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--optional">任意</span>
+            ユーザーネーム
+          </div>
+          <input type="text" name="name" class="form_input @error('name') form_input--err @enderror" value="">
+        </label>
+        <p class="form_errMsg">@error('name') {{ $message }} @enderror</p>
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            メールアドレス
+          </div>
+          <input type="text" name="email" class="form_input @error('email') form_input--err @enderror" value="">
+        </label>
+        <p class="form_errMsg">@error('email') {{ $message }} @enderror</p>
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--optional">任意</span>
+            プロフィール画像
+          </div>
+          <input type="text" name="image" class="form_input @error('image') form_input--err @enderror" value="">
+        </label>
+        <p class="form_errMsg">@error('image') {{ $message }} @enderror</p>
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">任意</span>
+            コメント
+          </div>
+          <input type="text" name="text" class="form_input @error('text') form_input--err @enderror" value="">
+        </label>
+        <p class="form_errMsg">@error('text') {{ $message }} @enderror</p>
+        <button type="submit" class="form_button" name="">更新して終わる</button>
+      </form>
+
+      <form action="{{ route('password.update') }}" method="post" class="form form--carousel">
+        @csrf
+        @method('put')
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            現在のパスワード
+          </div>
+          <input type="password" name="current_password" class="form_input @error('current_password') form_input--err @enderror" value="">
+        </label>
+        <p class="form_errMsg">@error('current_password') {{ $message }} @enderror</p>
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            新しいパスワード
+          </div>
+          <input type="password" name="password" class="form_input @error('password') form_input--err @enderror" value="">
+        </label>
+        <p class="form_errMsg">@error('password') {{ $message }} @enderror</p>
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            パスワード再入力
+          </div>
+          <input type="password" name="password_confirmation" class="form_input @error('password_confirmation') form_input--err @enderror" value="">
+        </label>
+        <p class="form_errMsg">@error('password_confirmation') {{ $message }} @enderror</p>
+        <button type="submit" class="form_button" name="">更新して終わる</button>
+      </form>
+
+      <div class="form form--carousel">
+        <button type="button" class="form_button">退会する（モーダルを開く）</button>
+      </div>
+
+      <form action="{{ route('profile.destroy') }}" method="post" class="form form--carousel">
+        @csrf
+        @method('delete')
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            パスワード確認
+          </div>
+          <input type="password" name="password" class="form_input @error('password') form_input--err @enderror" value="">
+        </label>
+        <p class="form_errMsg">@error('password') {{ $message }} @enderror</p>
+        <button type="submit" class="form_button" name="">更新して終わる</button>
+      </form>
+
     </div>
-</x-app-layout>
+  </div>
+</main>
+
+@endsection
