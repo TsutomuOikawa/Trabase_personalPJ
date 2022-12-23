5d978dc9513f55e312a8fda8917db56773cbc018
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-12-23 16:22:33 +0900

    update: パスワード再発行画面
    リセットメール送信画面および受信後の入力画面のフロント修正

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,28 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+@extends('layouts.app')
+@section('title', 'パスワードの再設定｜Trabase（トラベス）')
+@section('content')
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
+<main class="page-wrapper">
+  <div class="container--baseColor">
+    <h1 class="container_title">パスワードの再設定</h1>
+    <p class="container_notion">ご登録のメールアドレス宛に<br>再設定用リンクをお送りします</p>
+    <div class="container_body--xs">
+      <form method="post" action="{{ route('password.email') }}" class="form">
         @csrf
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            メールアドレス
+          </div>
+          <input type="text" name="email" class="form_input" value="" placeholder="example@test.com">
+        </label>
+        <p class="form_errMsg"></p>
+        <button type="submit" class="form_button" name="">メールを送る</button>
+        <p class="form_notion"><a href="{{ route('login') }}" class="link">&gt ログインフォームへ戻る</a></p>
+        <p class="form_notion"><a href="{{ route('register') }}" class="link">&gt ご登録がお済みでない方はこちら</a></p>
+      </form>
+    </div>
+  </div>
+</main>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@endsection
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,48 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@extends('layouts.app')
+@section('title', 'パスワードの再登録｜Trabase(トラベス)')
+@section('content')
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+    <main class="page-wrapper">
+      <div class="container--baseColor">
+        <h1 class="container_title">パスワードの再登録</h1>
+        <div class="container_body--xs">
+          <form method="POST" action="{{ route('password.store') }}" class="form">
+          @csrf
+            <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <label>
+              <div class="form_name">
+                <span class="form_label form_label--required">必須</span>
+                メールアドレス
+              </div>
+              <input type="text" name="email" class="form_input @error('email') form_input--err @enderror" value="old('email', $request->email)" placeholder="example@test.com">
+            </label>
+            <p class="form_errMsg">@error('email') {{ $message }} @enderror</p>
+
+            <label>
+              <div class="form_name">
+                <span class="form_label form_label--required">必須</span>
+                パスワード
+                <span class="smallfont">（半角英数字6文字以上）</span>
+              </div>
+              <input type="password" name="password" class="form_input @error('password') form_input--err @enderror" value="">
+            </label>
+            <p class="form_errMsg">@error('password') {{ $message }} @enderror</p>
+
+            <label>
+              <div class="form_name">
+                <span class="form_label form_label--required">必須</span>
+                パスワード再入力
+              </div>
+              <input type="password" name="password_confirmation" class="form_input @error('password_confirmation') form_input--err @enderror" value="">
+            </label>
+            <p class="form_errMsg">@error('password_confirmation') {{ $message }} @enderror</p>
+
+            <button type="submit" class="form_button" name="">ログインする</button>
+            <p class="form_notion"><a href="{{ route('forgot-password') }}" class="link">&gt パスワードをお忘れの方はこちら</a></p>
+            <p class="form_notion"><a href="{{ route('register') }}" class="link">&gt ご登録がお済みでない方はこちら</a></p>
+          </form>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+      </div>
+    </main>
+@endsection
