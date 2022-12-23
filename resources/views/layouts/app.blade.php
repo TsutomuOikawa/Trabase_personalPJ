98a3ff29a9d5600eb4f2eb5158d6c60d9ae44df4
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-12-23 12:19:23 +0900

    update: views/layouts/app.blade.php
    親テンプレートを修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,50 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'Trabase（トラベス）')</title>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Scripts -->
+    @vite(['resources/css/reset.css', 'resources/js/app.js', 'resources/sass/app.scss'])
+    <link rel="stylesheet" href="icon/css/all.css">
+  </head>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+  <body>
+    <header id="header" class="header js-change-header <?php if(strpos($_SERVER['PHP_SELF'],'index.php')||strpos($_SERVER['PHP_SELF'],'prefectures.php')) echo 'active';?>">
+      <div class="header-wrapper">
+        <a href="index.php" class="header_logo">Trabase</a>
+        <form class="header_form js-change-header_form <?php if(strpos($_SERVER['PHP_SELF'],'index.php')||strpos($_SERVER['PHP_SELF'],'prefectures.php')) echo 'nonactive';?>" action="index.html" method="post">
+          <input type="text" name="" class="header_input" value="" placeholder="都道府県名を入力">
+          <input type="text" name="" class="header_input" value="" placeholder="キーワードを入力">
+          <button type="submit" class="header_submit" name=""><i class="fas fa-search fa-lg"></i></button>
+        </form>
+        <div class="header_humburger js-menu-trigger">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-    </body>
+        <nav class="header_menu">
+          <ul class="menu">
+            <li class="menu_item"><a href="index.php#about" class="menu_item_link">About</a></li>
+            <li class="menu_item--sp"><a href="index.php#about" class="menu_item_link">Search Destination</a></li>
+            <li class="menu_item"><a href="login.php" class="menu_item_link">Login</a></li>
+            <li class="menu_item"><a href="register.php" class="menu_item_link">Register</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <!-- Page Content -->
+    @yield('content')
+
+
+    <footer id="footer" class="footer">
+      <small class="footer_copyright"> Copyright @ GlobeNote <br>All Rights Reserved</small>
+    </footer>
+    <script src="js/jquery-3.6.0.min.js" charset="utf-8"></script>
+    <script src="js/app.js" charset="utf-8"></script>
+  </body>
 </html>
