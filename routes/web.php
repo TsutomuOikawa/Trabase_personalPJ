2f42e9af2dfedb68db871ee6eb09200b128e9ab6
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2023-01-12 14:10:08 +0900

    clean: ルーティング整理

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,42 +15,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// ホーム画面
+// ホーム
 Route::get('/', \App\Http\Controllers\IndexController::class) -> name('index');
 // 都道府県別ページ
-Route::get('/prefecture/{id}', [\App\Http\Controllers\PrefectureController::class, 'showPref'])->whereNumber('id')->name('prefecture');
+Route::get('/pref/{pref_id}', [\App\Http\Controllers\PrefectureController::class, 'showPref'])->whereNumber('pref_id')->name('pref');
 
-// ノート一覧画面
-Route::get('/notes', [\App\Http\Controllers\NoteController::class, 'showList'])->name('notes.list');
-// ノート詳細閲覧
-Route::get('/notes/{note_id}', [\App\Http\Controllers\NoteController::class, 'showArticle'])->whereNumber('note_id')->name('notes.article');
+// ノート関連
+Route::controller(NoteController::class)->group(function() {
+    // ノート一覧画面
+    Route::get('/notes', 'showList')->name('notes.list');
+    // ノート詳細閲覧
+    Route::get('/notes/detail/{note_id}', 'showArticle')->whereNumber('note_id')->name('notes.article');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    // ログインユーザーのみ
+    Route::middleware('auth')->group(function () {
+        // ノート投稿画面
+        Route::get('/notes/new', 'new')->name('notes.new');
+        Route::post('/notes/new', 'storeNote')->name('notes.store');
+        // ノート再編集画面
+        Route::get('/notes/detail/{note_id}/edit', 'edit')->whereNumber('note_id')->name('notes.edit');
+        Route::post('/notes/detail/{note_id}/edit', 'getText')->whereNumber('note_id')->name('notes.getText');
+        Route::put('/notes/detail/{note_id}/edit', 'update')->whereNumber('note_id')->name('notes.update');
+        // ノート削除機能
+        Route::delete('/notes/detail/{note_id}/delete', 'delete')->whereNumber('note_id')->name('notes.delete');
+
+        // コメント投稿
+        Route::post('/notes/detail/{note_id}', 'storeComment')->whereNumber('note_id')->name('comment.storeComment');
+    });
+});
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // ノート投稿画面
-    Route::get('/notes/new', [\App\Http\Controllers\NoteController::class, 'new'])->name('notes.new');
-    Route::post('/notes/new', [\App\Http\Controllers\NoteController::class, 'storeNote'])->name('notes.store');
-    // ノート削除機能
-    Route::delete('/notes/{note_id}/delete', [\App\Http\Controllers\NoteController::class, 'delete'])->whereNumber('note_id')->name('notes.delete');
-
-    // ノート再編集画面
-    Route::get('/notes/{note_id}/edit', [\App\Http\Controllers\NoteController::class, 'edit'])->whereNumber('note_id')->name('notes.edit');
-    Route::post('/notes/{note_id}/edit', [\App\Http\Controllers\NoteController::class, 'getText'])->whereNumber('note_id')->name('notes.getText');
-    Route::put('/notes/{note_id}/edit', [\App\Http\Controllers\NoteController::class, 'update'])->whereNumber('note_id')->name('notes.update');
-
-    // コメント投稿
-    Route::post('/notes/{note_id}', [\App\Http\Controllers\CommentController::class, 'storeComment'])->whereNumber('note_id')->name('comment.storeComment');
-
     // マイページ
     Route::get('mypage', [\App\Http\Controllers\MypageController::class, 'mypage'])->name('mypage');
+    // プロフィール編集
+    Route::get('mypage/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('mypage/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('mypage/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
