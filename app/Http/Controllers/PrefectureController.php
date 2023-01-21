12361786dd1b2d0500121527c5abae3c05b227c2
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2023-01-21 14:59:25 +0900

    update:upgrade: prefノートのお気に入り動的化
    データの取得方法をeloquentからクエリビルダに変更（共通関数化を見越して）
    それに伴ってbladeファイルも修正

--- app/Http/Controllers/PrefectureController.php
+++ app/Http/Controllers/PrefectureController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Note;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\NoteController;
 use App\Models\Prefecture;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PrefectureController extends Controller
 {
@@ -21,21 +24,22 @@ class PrefectureController extends Controller
           $prefs = Prefecture::all();
           $data = $prefs->find($pref_id);
           // ノートデータ
-          $notes = Note::with('user')
-                        ->with('prefecture')
-                        ->where('pref_id', $pref_id)
-                        ->orderBy('note_id', 'DESC')
-                        ->take(8)
-                        ->get();
+          $notes = DB::table('notes')
+                      ->leftJoin('users', 'notes.user_id', '=', 'users.id')
+                      ->where('notes.pref_id', $pref_id)
+                      ->select('notes.*', 'users.id', 'users.name', 'users.avatar', 'users.intro')
+                      ->orderBy('note_id', 'DESC')
+                      ->limit(8)
+                      ->get();
+          foreach ($notes as $note) {
+            $note->isFavorite = FavoriteController::isFavorite(Auth::id(), $note->id);
+            $note = NoteController::countFavsAndComs($note);
+          }
+
           return view('prefecture')
             ->with('prefs', $prefs)
             ->with('data', $data)
             ->with('notes', $notes);
         }
-    }
-
-    public static function getPrefs()
-    {
-      return Prefecture::all();
     }
 }
