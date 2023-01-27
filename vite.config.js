a401d620e13e8e11961a99fd3a60a5fde582f100
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2023-01-27 12:34:17 +0900

    update: viteファイル設定

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/jquery.japan-map.min.js',
                 'resources/sass/app.scss'
             ],
             refresh: true,
