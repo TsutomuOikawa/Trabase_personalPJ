8239b5af0929d5816af7865f2517bdd29b0fe468
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2023-01-30 17:02:53 +0900

    update: モーダルウィンドウのコンポーネント化

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,10 @@
+<div class="modal js-modal">
+  <div class="modal_content">
+    <p class="modal_header">{{ $modal_title }}</p>
+
+    {{ $modal_content }}
+
+    <p class="modal_action js-hide-modal">{{ $modal_action }}</p>
+  </div>
+  <div class="modal_cover"></div>
+</div>
