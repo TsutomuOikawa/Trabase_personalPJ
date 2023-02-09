8eb1844a2fde3f74a70cd90c8f9347879092b923
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2023-02-09 23:53:06 +0900

    fix

--- resources/views/prefecture.blade.php
+++ resources/views/prefecture.blade.php
@@ -51,7 +51,7 @@
     <h2 class="container_title">行き先を変える</h2>
     <div class="container_body">
       <div class="list--destination">
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-regular fa-square-minus"></i>北海道・東北</h3>
+        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-minus"></i>北海道・東北</h3>
         <div class="list_body--destination js-toggle-list js-active">
           @foreach($prefs as $pref)
           <a href="{{ route('pref', ['pref_id' => $pref->pref_id]) }}" class="panel--destination js-change-back">
@@ -63,32 +63,32 @@
           @switch($pref->pref_id)
           @case(7)
         </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-regular fa-square-plus"></i>関東</h3>
+        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>関東</h3>
         <div class="list_body--destination js-toggle-list">
           @break
           @case(14)
         </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-regular fa-square-plus"></i>北陸・甲信越</h3>
+        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>北陸・甲信越</h3>
         <div class="list_body--destination js-toggle-list">
           @break
           @case(20)
         </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-regular fa-square-plus"></i>東海</h3>
+        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>東海</h3>
         <div class="list_body--destination js-toggle-list">
           @break
           @case(24)
         </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-regular fa-square-plus"></i>近畿</h3>
+        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>近畿</h3>
         <div class="list_body--destination js-toggle-list">
           @break
           @case(30)
         </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-regular fa-square-plus"></i>中国・四国</h3>
+        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>中国・四国</h3>
         <div class="list_body--destination js-toggle-list">
           @break
           @case(39)
         </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-regular fa-square-plus"></i>九州・沖縄</h3>
+        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>九州・沖縄</h3>
         <div class="list_body--destination js-toggle-list">
 
           @default
