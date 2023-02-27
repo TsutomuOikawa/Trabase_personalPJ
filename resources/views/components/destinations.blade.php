32b23794354ee2dd9dbb97fe73af4f36b0b7ef89
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2023-02-27 13:43:52 +0900

    update: 行先リストのコンポーネント化

---
+++ resources/views/components/destinations.blade.php
@@ -0,0 +1,52 @@
+<div class="list--destination">
+  <{{ $tag }} class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-minus"></i>北海道・東北</{{ $tag }}>
+  <div class="list_body--destination js-toggle-list js-active">
+  @foreach($prefs as $pref)
+    <a href="{{ route('pref', ['pref_id' => $pref->pref_id]) }}" class="panel--destination js-change-back">
+      <img src="{{ Storage::disk('s3')->url('assets/hero/'.$pref->pref_name.'.jpg') }}" class="panel_destImg" alt="{{ $pref->pref_name }}のイメージ写真">
+      <span class="panel_destCover">
+        <span class="panel_destName">{{ $pref->pref_name }}</span>
+      </span>
+    </a>
+
+  @switch($pref->pref_id)
+    @case(7)
+  </div>
+  <{{ $tag }} class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>関東</{{ $tag }}>
+  <div class="list_body--destination js-toggle-list">
+    @break
+
+    @case(14)
+  </div>
+  <{{ $tag }} class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>北陸・甲信越</{{ $tag }}>
+  <div class="list_body--destination js-toggle-list">
+    @break
+
+    @case(20)
+  </div>
+  <{{ $tag }} class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>東海</{{ $tag }}>
+  <div class="list_body--destination js-toggle-list">
+    @break
+
+    @case(24)
+  </div>
+  <{{ $tag }} class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>近畿</{{ $tag }}>
+  <div class="list_body--destination js-toggle-list">
+    @break
+
+    @case(30)
+  </div>
+  <{{ $tag }} class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>中国・四国</{{ $tag }}>
+  <div class="list_body--destination js-toggle-list">
+    @break
+
+    @case(39)
+    </div>
+    <{{ $tag }} class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>九州・沖縄</{{ $tag }}>
+    <div class="list_body--destination js-toggle-list">
+
+    @default
+  @endswitch
+  @endforeach
+  </div>
+</div>
--- resources/views/prefecture.blade.php
+++ resources/views/prefecture.blade.php
@@ -50,52 +50,9 @@
   <section id="destination" class="container--baseColor destination js-change-back-target">
     <h2 class="container_title">行き先を変える</h2>
     <div class="container_body">
-      <div class="list--destination">
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-minus"></i>北海道・東北</h3>
-        <div class="list_body--destination js-toggle-list js-active">
-          @foreach($prefs as $pref)
-          <a href="{{ route('pref', ['pref_id' => $pref->pref_id]) }}" class="panel--destination js-change-back">
-            <img src="{{ Storage::disk('s3')->url('assets/hero/'.$pref->pref_name.'.jpg') }}" class="panel_destImg" alt="{{ $pref->pref_name }}のイメージ写真">
-            <span class="panel_destCover">
-              <span class="panel_destName">{{ $pref->pref_name }}</span>
-            </span>
-          </a>
-          @switch($pref->pref_id)
-          @case(7)
-        </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>関東</h3>
-        <div class="list_body--destination js-toggle-list">
-          @break
-          @case(14)
-        </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>北陸・甲信越</h3>
-        <div class="list_body--destination js-toggle-list">
-          @break
-          @case(20)
-        </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>東海</h3>
-        <div class="list_body--destination js-toggle-list">
-          @break
-          @case(24)
-        </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>近畿</h3>
-        <div class="list_body--destination js-toggle-list">
-          @break
-          @case(30)
-        </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>中国・四国</h3>
-        <div class="list_body--destination js-toggle-list">
-          @break
-          @case(39)
-        </div>
-        <h3 class="list_title js-switch-toggle-list"><i class="fa-solid fa-square-plus"></i>九州・沖縄</h3>
-        <div class="list_body--destination js-toggle-list">
-
-          @default
-          @endswitch
-          @endforeach
-        </div>
-      </div>
+      @component('components.destinations',
+        ['tag' => 'h3', 'prefs' => $prefs])
+      @endcomponent
     </div>
   </section>
   <div class="followingBtn js-show-modal">
