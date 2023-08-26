f7941c00237b805f78b682df60604d2d272491b5
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2023-08-26 18:45:42 +0900

    マイページのjapanMapをjQuery→TypeScript化
    jsファイルにHTMLをベタ書きしていた箇所も修正

--- src/resources/views/components/modal.blade.php
+++ src/resources/views/components/modal.blade.php
@@ -1,10 +1,9 @@
-<div class="modal js-modal">
-  <div class="modal_content">
-    <p class="modal_header">{{ $modal_title }}</p>
-
-    {{ $modal_content }}
-
-    <p class="modal_action js-hide-modal">{{ $modal_action }}</p>
-  </div>
-  <div class="modal_cover"></div>
+@vite(['resources/ts/parts/modal.ts'])
+<div class="modal ts-modal">
+    <div class="modal_content">
+        <p class="modal_header">{{ $modal_title }}</p>
+        {{ $modal_content }}
+        <p class="modal_action ts-hide-modal">{{ $modal_action }}</p>
+    </div>
+    <div class="modal_cover"></div>
 </div>
